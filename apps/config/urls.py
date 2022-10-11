13a08a8890daa979efb7674d6e9b21024de66ab6
Casey Vu <pewgfiwr2@protonmail.ch>
2022-10-10 19:29:21 -0700

    Updated config.urls to use tracker.urls.

--- apps/config/urls.py
+++ apps/config/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tracker/', include('tracker.urls')),
+    path('', include('tracker.urls')),
 ]
