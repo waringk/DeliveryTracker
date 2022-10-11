4b77758ed72ea3463f30f7ebcab5e1714425b0c2
Casey Vu <pewgfiwr2@protonmail.ch>
2022-10-10 19:54:27 -0700

    Update urls.py in tracker to use home.html.

--- apps/tracker/urls.py
+++ apps/tracker/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
+from .views import HomePageView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", HomePageView.as_view(), name="home"),
 ]
