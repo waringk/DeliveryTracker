cadf9f86a90ece3980dca03bd91696724408aae5
Casey Vu <pewgfiwr2@protonmail.ch>
2022-10-11 16:42:00 -0700

    Created SignUpView class with CreateView and signup.html. Redirected the user to the login page after successful  registration.

--- apps/tracker/views.py
+++ apps/tracker/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
 
 class HomePageView(TemplateView):
-    # Specify template name to use app level home.html
+    # Specify template name to use app level home.html.
     template_name = "app-tracker/home.html"
+
+
+class SignUpView(CreateView):
+    # Use Django's generic Creation form for the signup.
+    form_class = UserCreationForm
+    # Redirect the user to the login page after signing up successfully.
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
