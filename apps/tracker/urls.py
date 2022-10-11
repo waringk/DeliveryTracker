624b4879aa7e5185c4fdf842a3eb1bb90d0e5af7
Casey Vu <pewgfiwr2@protonmail.ch>
2022-10-11 16:33:27 -0700

    Imported SignUp page in the tracker urls.py.

--- apps/tracker/urls.py
+++ apps/tracker/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
-from .views import HomePageView
-from . import views
+from .views import HomePageView, SignUpView
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('', HomePageView.as_view(), name='home'),
 ]
