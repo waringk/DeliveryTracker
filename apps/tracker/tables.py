eeb04fc3e965f7d6f8107eca5a1c03a7b22372d7
Casey Vu <pewgfiwr2@protonmail.ch>
2022-10-17 18:42:38 -0700

    Updated EventTable to exclude id from columns. Created PhotoTables. Changed image size.

--- apps/tracker/tables.py
+++ apps/tracker/tables.py
@@ -1,8 +1,32 @@
 import django_tables2 as tables
-from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
 from .models import Event
+
+
+class PhotoTable(tables.Table):
+    # Creates the columns for model attributes
+    id = tables.Column()
+    photo = tables.Column()
+    created = tables.Column()
+    user = tables.Column()
+
+    def render_photo(self, value, record):
+        # Render the photo as a linkable image in the column
+        return mark_safe(self.linkify(
+            "<img src='" + value.url + "' width='480' height='200' />",
+            record) + " " + self.linkify('<br>Event Details', record))
+
+    def linkify(self, text, record):
+        # Render the event link in the column to events' details page
+        return mark_safe(f"<a href=" + str(record.id) + " >" + text + "</a>")
+
+    class Meta:
+        model = Event
+        template_name = "django_tables2/bootstrap4.html"
+        sequence = ("photo", "created", "user",)
+        fields = ("photo", "created", "user",)
+        exclude = ("id",)
 
 
 class EventTable(tables.Table):
@@ -14,13 +38,17 @@ class EventTable(tables.Table):
 
     def render_photo(self, value, record):
         # Render the photo as a linkable image in the column
-        return mark_safe(self.linkify("<img src='" + value.url + "' width='200' height='200' />", record)+" "+self.linkify('<br>Event Details', record))
+        return mark_safe(self.linkify(
+            "<img src='" + value.url + "' width='400' height='200' />",
+            record) + " " + self.linkify('<br>Event Details', record))
 
     def linkify(self, text, record):
         # Render the event link in the column to events' details page
-        return mark_safe(f"<a href=" + str(record.id) + " >"+text+"</a>")
+        return mark_safe(f"<a href=" + str(record.id) + " >" + text + "</a>")
 
     class Meta:
         model = Event
-        template_name = "django_tables2/bootstrap.html"
+        template_name = "django_tables2/bootstrap4.html"
+        sequence = ("photo", "created", "user",)
         fields = ("photo", "created", "user",)
+        exclude = ("id",)
