5fc1c4957afd5cb5d11b16987d4e07c0ae097c86
8Gates <cdsmith7@gmail.com>
2022-10-18 21:14:19 -0500

    Deleted duplicate lines

--- apps/config/settings.py
+++ apps/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 import environ
+from cryptography.fernet import Fernet
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,8 +149,14 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
 # SMTP Email Configuration
 # https://suhailvs.github.io/blog02.html#mail-setup-on-django-using-gmail
 # new host emails must also unlock captcha
+key = ''
+fernet = Fernet(key)
+encryptedPassword = b'gAAAAABjT1itzEEg22vK' \
+       b'-R85lhXTEcpPjNy6ll6A8NcWg4bejcqqo6dBdrDcBJsOo1wRorQp82Fry8O-l6Raa-' \
+                    b'3518VX6kMcy7gUV-DTP5v-EwBatMGU4tQ='
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'deliverytrackerteam@gmail.com'
-EMAIL_HOST_PASSWORD = 'ifxgdbtaryihswrb'
+EMAIL_HOST_PASSWORD = fernet.decrypt(encryptedPassword).decode()
