41c506ff5c3d9c517b66e63f8c06c7a429a27afc
Casey Vu <pewgfiwr2@protonmail.ch>
2022-10-20 16:40:57 -0700

    Updated admin panel for Events to display created date and date filtering.

--- apps/tracker/admin.py
+++ apps/tracker/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+
 from .models import Event
 
-# Register your models here.
-admin.site.register(Event)
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'created', 'photo', 'user']
+    list_filter = ['created', 'user']
+    raw_id_fields = ['user']
+    date_hierarchy = 'created'
+    ordering = ['-created']
