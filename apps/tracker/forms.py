2342f7991aaa1665bb51ef9b1c439cb3c49d7b2a
waringk <kathleenwaring@hotmail.com>
2022-10-19 21:53:23 -0400

    Created Events Default Viewing Behavior and Calendar Widget
    Added default behavior for Events page to display the most recent uploaded day's events from the user's list of events in their account. Added sorting to display the most recent event if there are multiple in one day. Added calendar widget to allow user to select date and redirect's user to display the selected date events.

--- apps/tracker/forms.py
+++ apps/tracker/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.admin.widgets import AdminDateWidget
+from django.forms import ModelForm, Form
+from .models import Event
 
 
 class UserRegisterForm(UserCreationForm):
@@ -13,3 +16,18 @@ class UserRegisterForm(UserCreationForm):
             "username",
             "email",
         ]
+
+
+class DateInput(forms.DateInput):
+    # Date Input for selecting Events by date
+    input_type = 'date'
+
+
+class DateForm(ModelForm):
+    # Calendar Form and Widget for selecting Events by date
+    class Meta:
+        model = Event
+        fields = ['created']
+        widgets = {
+            'created': DateInput(),
+        }
