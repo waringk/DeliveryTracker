24fb49bd96a6eb14fb1532501a8065180c601e08
Casey Vu <pewgfiwr2@protonmail.ch>
2022-10-24 21:52:38 -0700

    Added reset password email path. Added one-time password reset link path. Added password reset complete path.

--- apps/tracker/urls.py
+++ apps/tracker/urls.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -8,13 +7,21 @@ urlpatterns = [
     path('change_password/', views.PasswordsChangeView.as_view(
         template_name="registration/change_password.html")),
     path('password_success', views.password_success, name='password_success'),
-    path('reset_password/', views.PasswordsResetView.as_view(
-        template_name="registration/reset_password.html")),
+    path('reset_password/',
+         views.PasswordsResetView.as_view(
+             template_name="registration/reset_password.html"),
+         name="reset_password"),
     path('reset_password', views.reset_password, name='reset_password'),
     path('reset_password/done/',
          views.PasswordsResetDoneView.as_view(
              template_name="registration/reset_password_done.html"),
          name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', views.PasswordsResetConfirmView.as_view(
+        template_name="registration/reset_password_confirm.html"),
+         name="reset_password_confirm"),
+    path('reset_password_complete/', views.PasswordsResetCompleteView.as_view(
+        template_name="registration/reset_password_complete.html"),
+         name="password_reset_complete"),
     path('', views.HomePageView.as_view(), name='home'),
     path('uploadFrame/', views.upload_frame, name='uploadFrame'),
     path('events/', views.EventListView.as_view(), name='events'),
