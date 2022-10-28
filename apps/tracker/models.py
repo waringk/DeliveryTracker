a01bc17bf102fb9686108e2634d9f08568bbb789
IWR2 <pewgfiwr2@protonmail.ch>
2022-10-27 18:56:26 -0700

    Commented out create_user_device signal. Removed null and blank attributes from uuid in UserDevice model.

--- apps/tracker/models.py
+++ apps/tracker/models.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save
+#from django.db.models.signals import post_save
 from django.utils import timezone
-from django.dispatch import receiver
-
+#from django.dispatch import receiver
 
 class Event(models.Model):
     photo = models.ImageField(upload_to='images/')
@@ -23,14 +22,16 @@ class Event(models.Model):
 
 class UserDevice(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    uuid = models.CharField(max_length=36, null=True, blank=True)
+    # uuid = models.CharField(max_length=36, null=True, blank=True)
+    uuid = models.CharField(max_length=36)
 
     def __str__(self):
         return self.user.username
 
-
-@receiver(post_save, sender=User)
-def create_user_device(sender, instance, created, **kwargs):
-    # if user successfully added to User table, create device
-    if created:
-        UserDevice.objects.create(user=instance)
+# @receiver(post_save, sender=User)
+# def create_user_device(sender, instance, created, **kwargs):
+#     # if user successfully added to User table, create device
+#     if created:
+#         # To create a user device object
+#         UserDevice.objects.get_or_create(user=instance)
+#
